58207ae2be2a9682a63095895abd4d3b7897ba14
Omar Jalalzada <omar@Omars-iMac.local>
2017-09-17 10:53:09 -0700

    time for the weekend update

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,23 @@
-// JS Goes here - ES6 supported
+// Get browser height and apply it to the index container on the homepage
+// the height is dynamic to align the spark line with the first image properly
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/js/app.js");
+var ojWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var ojHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+// console.log("width: " + ojWidth);
+// console.log("height: " + ojHeight);
+
+if (ojHeight > 960) {
+  document.getElementById("index_container").className += " index_imac";
+} else if (ojHeight < 960 & ojHeight > 700 ) {
+  document.getElementById("index_container").className += " index_macbook";
+} else if (ojHeight < 700) {
+  document.getElementById("index_container").className += " index_iphone";
+} else {
+
+}
+// Ending get browser height
+
+
+
+
