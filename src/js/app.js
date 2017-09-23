10d4ca7f70ca4691c0087205f1e4eac8e1c8895e
Omar Jalalzada <omar@Omars-iMac.local>
2017-09-22 23:12:18 -0700

    getting close to finalize the homepage

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ if (ojHeight > 960) {
 
 
 // JavaScript
-window.sr = ScrollReveal({ duration: 1000, scale: 1, distance: '10px', viewFactor: 0.01 });
+window.sr = ScrollReveal({ duration: 2000, scale: 1, distance: '10px', viewFactor: 0.03, easing: 'cubic-bezier(.18,.33,.59,1)' });
 // sr.reveal('.img2_container', { distance: '20px', duration: 600, origin: 'left', delay: 200 });
 // sr.reveal('.img1_container', { distance: '20px', duration: 600, origin: 'right', delay: 200 });
 // sr.reveal('.img3_container', { distance: '10px', duration: 600 });
@@ -41,5 +41,12 @@ window.sr = ScrollReveal({ duration: 1000, scale: 1, distance: '10px', viewFacto
 
 sr.reveal('.sr_direction_left', { origin: 'right' });
 sr.reveal('.sr_direction_right', { origin: 'left' });
+sr.reveal('.sr_direction_down', { origin: 'down' });
 sr.reveal('.sr_direction_up');
 sr.reveal('.sr_delay_100', { delay: 100 });
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("sparkie").className += " loaded";
+});
